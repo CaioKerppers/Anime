dffd83f0dba47905255464441e324b2ad5190b27
LorenzoPrograming <lm.pilati@gmail.com>
2023-09-10 11:15:23 -0300

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'anime-cdde9',
+    appId: '1:200735475258:web:9668366e8bcee4087e9703',
+    storageBucket: 'anime-cdde9.appspot.com',
+    apiKey: 'AIzaSyD5v0q7ylaPbETn5ImLIY7QwpDQf6MlKr4',
+    authDomain: 'anime-cdde9.firebaseapp.com',
+    messagingSenderId: '200735475258',
+  },
   production: false
 };
 
